503d90d204f65274a7eddef34c8c5908ce5aa5b9
fkarg <f.karg10@gmail.com>
2020-04-10 17:35:27 +0200

    JT changes

--- main.py
+++ main.py
@@ -23,8 +23,9 @@ def loadValues():
         print("Failed loading value set: {}".format(str(e)))
         return {}
 
+
 def getSessionFilePath():
-    sessions = glob.glob('.*.session')
+    sessions = glob.glob(".*.session")
     print("Present Sessions:")
     print("[0] New Session")
     for i, path in enumerate(sessions):
@@ -49,15 +50,16 @@ def clear(text=""):
         print(text)
 
 
-def get_input() -> int:
+def get_input(min_value, max_value) -> int:
     """ Returns a number, if one got 'inputted'.
     Might throw a KeyboardInterrupt.
 
     Args:
-        None
+        min_value: lower bound
+        max_value: upper bound
 
     Returns:
-        int: number between 0 and 3
+        int: number between min_value and max_value inclusive
 
     Throws:
         KeyboardInterrupt, if the user intends to end inputting values
@@ -73,44 +75,52 @@ def get_input() -> int:
 
     try:
         index = int(inp)
-        assert index > -1
-        assert index < 4
+        assert index >= min_value
+        assert index <= max_value
     except (ValueError, AssertionError):
         print(
             "Please enter '1', '2' or '3' to select one of the presented values, or '0' if they are of equal value to you."
         )
-        return get_input()  # this might lead to a very unlikely
+        return get_input(min_value, max_value)  # this might lead to a very unlikely
         # 'recursion limit error' when repeated more than 5k times.
-        # (or whatever is the limit in your current python runtime)
     return index
 
 
-def main(showDescr=True):
+def main(showDescr = True):
     print("welcome to the core value finder")
     values = {}
     valueset = loadValues()
+    count = 0
 
     sessionPath = getSessionFilePath()
     try:
         with open(sessionPath) as json_file:
             data = json.load(json_file)
             values = data["values"]
+            count = data["count"]
     except Exception as e:
         print("Failed loading Session: {}".format(str(e)))
         print("Fallback to new Session")
         sessionPath = None
 
     if not sessionPath:
-        sessionPath = ".{}.session".format(time.strftime("%Y-%m-%d-%H:%M"))
+        sessionPath = ".{}.session".format(time.strftime("%Y-%m-%d-%H-%M"))
         print("New Session: {}".format(sessionPath))
         for v in valueset.keys():
             values[v] = 0
+    input("Press Return to start")
 
-    # 2^len(values) comparisons are a good estimate to get solid value differences.
-    # can be aborted prior to that, and repeated.
-    for _ in range(2 ** len(values)):
-        selection = [choice([*values]), choice([*values]), choice([*values])]
+    for _ in range(len(values) ** 2):
+        # select shown values
+        selection = []
+        for _ in range(0, 3):
+            while len(values) > len(selection):
+                newValue = choice([*values])
+                if newValue not in selection:
+                    selection += [newValue]
+                    break
         clear()
+        print(f"Round: {count}")
         if showDescr:
             for i in range(0, len(selection)):
                 valueName = selection[i]
@@ -127,21 +137,27 @@ def main(showDescr=True):
             )
 
         try:
-            index = get_input()
+            index = get_input(0, len(selection))
             if index:  # if index == 0: don't evaluate
                 values[selection[index - 1]] += 1
+                count += 1
         except KeyboardInterrupt:
 
             print("Saving Session: {}".format(sessionPath))
-            dump = {"values": values, "timestamp": time.strftime("%Y%m%d-%H%M%S")}
+            dump = {
+                "values": values,
+                "timestamp": time.strftime("%Y%m%d-%H%M%S"),
+                "count": count,
+            }
             json_data = json.dumps(dump, indent=2)
             with open(sessionPath, "w") as file:
                 file.write(json_data)
             break
 
     sorted_x = sorted(values.items(), key=operator.itemgetter(1), reverse=True)
-    clear("Values sorted by number of times they 'outcompeted' others.")
-    print("A total of {0} comparisons has been done.\n".format(sum(values.values())))
+    clear()
+    print("Values sorted by number of times they 'outcompeted' others.")
+    print(f"A total of {count} comparisons has been done.\n")
     print("----- ---------------")
     for k, v in sorted_x:
         print("{1:>4}: {0}".format(k, v))
